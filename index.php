5d4bddb5358aaf5580a36463ebba2c4aebbf4519
retrocode <630413133@qq.com>
2022-01-07 01:01:57 +0800

    1.抽取导航链接到txt文件中 2.添加 在线markdown工具 链接

--- index.php
+++ index.php
@@ -14,6 +14,35 @@ if ($q == "" || $q == null) {
         echo '<script>window.open("//www.baidu.com/s?ie=utf-8&word=' . $q . '","_blank");</script>';
     }
 };
+
+function get_file($file){
+    $data=file_get_contents($file);
+    $linkList=array();
+    $lines=explode("\n",$data);
+    foreach ($lines as $line) {
+        if (strlen($line)==0) continue;
+        $detail = str_replace("\r", '', $line);
+        array_push($linkList, $detail);
+    }
+    return $linkList;
+}
+
+function echo_link($list){
+    for ($x = 0; $x <= count($list); $x++) {
+        $tmpstr = $list[$x];
+        if (substr($tmpstr, 0, 1) == "#"){
+            // 此行不输出
+        }else if (substr($tmpstr, 0, 4) == "    "){
+            $tmpstr = substr($tmpstr, 4, strlen($tmpstr));
+            $tmplink = explode(" ",$tmpstr);
+            echo '<li><a rel="nofollow" href="'.$tmplink[2].'" target="_blank"><i class="iconfont '.$tmplink[1].'" style="'.implode("",array_slice($tmplink, 3, count($tmplink))).'"></i>'.$tmplink[0].'</a></li>';
+        }else if ($tmpstr!=""){
+            $tmptitle = explode(" ",$tmpstr);
+            echo '<li class="title"><i class="iconfont '.$tmptitle[1].'"></i>'.$tmptitle[0].'</li>';
+        }
+    }
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="zh-CN">
@@ -40,7 +69,7 @@ if ($q == "" || $q == null) {
     <title>搜索</title>
     <link href="style.css?t=<?php echo date("ymdhi"); ?>" rel="stylesheet">
     <!-- 我自己的CSS图标 -->
-    <link rel="stylesheet" href="font_1654770_rod4ek8x7t.css">
+    <link rel="stylesheet" href="font_1654770.css?t=<?php echo date("ymdhi"); ?>">
     <!--<link rel="stylesheet" href="//at.alicdn.com/t/font_1654770_gsrv0hc1am.css">-->
     <!-- 现已将前端重构,剔除掉了jquery,使用原生js实现响应功能,体积精简31kb -->
     <!--<script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>-->
@@ -77,55 +106,7 @@ if ($q == "" || $q == null) {
 <body class="skin-container" topmargin="0" oncontextmenu="return false" ondragstart="return false" onselectstart="return false" onselect="document.getSelection().empty()" oncopy="document.getSelection().empty()" onbeforecopy="return false">
     <div id="menu"><i></i></div>
     <div class="list closed">
-        <ul>
-            <!------>
-            <li class="title"><i class="iconfont icon-kongzhitai"></i> 开发</li>
-            <li><a rel="nofollow" href="//retrocode.io/" target="_blank"><i class="iconfont icon-boke-copy"></i>我的博客</a></li>
-            <li><a rel="nofollow" href="//github.com/" target="_blank"><i class="iconfont icon-git"></i>Github</a></li>
-            <li><a rel="nofollow" href="//gitee.com/" target="_blank"><i class="iconfont icon-mayun" style="color:#f03;"></i>码云</a></li>
-            <li><a rel="nofollow" href="//juejin.im/" target="_blank"><i class="iconfont icon-juejin" style="color: #0084ff;"></i>掘金</a></li>
-            <li><a rel="nofollow" href="//www.ituring.com.cn/" target="_blank"><i class="iconfont icon-tulingdianzika" style="color: #0084ff;"></i>图灵社区</a></li>
-            <li><a rel="nofollow" href="//uniapp.dcloud.io/" target="_blank"><i class="iconfont icon-uni-app" style="color: green;"></i>Uni-App</a></li>
-            <li><a rel="nofollow" href="//www.apicloud.com/" target="_blank"><i class="iconfont icon-APICloud" style="color: #0084ff;"></i>ApiCloud</a></li>
-            <li><a rel="nofollow" href="//taro.aotu.io/" target="_blank"><i class="iconfont icon-React" style="color: #0084ff;"></i>Taro</a></li>
-            <!--<li><a rel="nofollow" href="https://www.52pojie.cn/" target="_blank"><i class="iconfont icon-theater-masks" style="color:#f03;"></i>吾爱破解</a></li>-->
-            <li><a rel="nofollow" href="//www.bootcdn.cn/" target="_blank"><i class="iconfont icon-cdn" style="color: #e52;"></i>Bootcdn</a></li>
-            <li><a rel="nofollow" href="//dash.cloudflare.com/" target="_blank"><i class="iconfont icon-cloudflare" style="color: #f37f20;"></i>Cloudflare</a></li>
-            <li><a rel="nofollow" href="//sg.godaddy.com/" target="_blank"><i class="iconfont icon-domainyuming" style="color: #f37f20;"></i>Godaddy</a></li>
-            <li><a rel="nofollow" href="//v2ex.com/" target="_blank"><i class="iconfont icon-vex" style="color: #303030;"></i>V2ex</a></li>
-            <!------>
-            <li class="title"><i class="iconfont icon-tieba1"></i> 贴吧</li>
-            <li><a rel="nofollow" href="//tieba.baidu.com/f?kw=wp7" target="_blank"><i class="iconfont icon-tieba1"></i>WP7吧</a></li>
-            <li><a rel="nofollow" href="//tieba.baidu.com/f?kw=steam" target="_blank"><i class="iconfont icon-tieba1"></i>Steam吧</a></li>
-            <!------>
-            <li class="title"><i class="iconfont icon-xingzhuang"></i> 邮箱</li>
-            <li><a rel="nofollow" href="//mail.qq.com/" target="_blank"><i class="iconfont icon-QQyouxiang" style="color: #f05;"></i>QQ邮箱</a></li>
-            <!------>
-            <li class="title"><i class="iconfont icon-xiaoxi"></i> 社交</li>
-            <li><a rel="nofollow" href="//www.zhihu.com/" target="_blank"><i class="iconfont icon-zhihu" style="color: #0084ff;"></i>知乎</a></li>
-            <li><a rel="nofollow" href="//www.gamersky.com/" target="_blank"><i class="iconfont icon-youminxingkong" style="color:#f03;"></i>游民星空</a></li>
-            <li><a rel="nofollow" href="//www.ltaaa.com/" target="_blank"><i class="iconfont icon-long" style="color:#0084ff;"></i>龙腾网</a></li>
-            <!------>
-            <li class="title"><i class="iconfont icon-shi-pin-xin-wen"></i> 视频媒体</li>
-            <li><a rel="nofollow" href="//www.youtube.com/" target="_blank"><i class="iconfont icon-yout" style="color:#f03;"></i>Youtube</a></li>
-            <li><a rel="nofollow" href="//www.acfun.cn/" target="_blank"><i class="iconfont icon-acfun" style="color:#f33;"></i>ACFUN</a></li>
-            <li><a rel="nofollow" href="//www.bilibili.com/" target="_blank"><i class="iconfont icon-CN_bilibili" style="color:#09e;"></i>哔哩哔哩</a></li>
-            <li><a rel="nofollow" href="/movies/" target="_blank"><i class="iconfont icon-yunbo" style="color:#0bf;"></i>影视搜索</a></li>
-            <li><a rel="nofollow" href="//music.yyv.me/" target="_blank"><i class="iconfont icon-wangyiyun" style="color:#f03;"></i>网易云音乐</a></li>
-            <!------>
-            <li class="title"><i class="iconfont icon-che"></i> 购物</li>
-            <li><a rel="nofollow" href="//www.taobao.com" target="_blank"><i class="iconfont icon-taobao" style="color: #ff6019;"></i>淘宝网</a></li>
-            <li><a rel="nofollow" href="//www.jd.com" target="_blank"><i class="iconfont icon-jingdong" style="color: #e33333;"></i>京东</a></li>
-            <!------>
-            <li class="title"><i class="iconfont icon-sheji"></i> 设计视觉</li>
-            <li><a rel="nofollow" href="//www.iconfont.cn/" target="_blank"><i class="iconfont icon-print" style="color: #ff6019;"></i>阿里图标</a></li>
-            <!------>
-            <li class="title"><i class="iconfont icon-tool"></i> 工具</li>
-            <li><a rel="nofollow" href="//cv.retrocode.io/" target="_blank"><i class="iconfont icon-zimu" style="color: #067;"></i>临时公告板</a></li>
-            <li><a rel="nofollow" href="//fanyi.baidu.com/" target="_blank"><i class="iconfont icon-fanyi" style="color: #02f;"></i>百度翻译</a></li>
-            <li><a rel="nofollow" href="//pan.baidu.com/disk/main" target="_blank"><i class="iconfont icon-baiduyun" style="color: #148bfe;"></i>百度网盘</a></li>
-            <li><a rel="nofollow" href="//ping.chinaz.com/" target="_blank"><i class="iconfont icon-pingup" style="color:#2361ad;"></i>站长Ping</a></li>
-        </ul>
+        <ul><?php echo_link(get_file("links.txt")); ?></ul>
     </div>
     <div class="mywth">
         <div class="wea_hover">
@@ -185,5 +166,4 @@ if ($q == "" || $q == null) {
 <!-- 实测这个动画效果,占用的CPU过多,需要硬性要求浏览器开启硬件加速,故暂时屏蔽 -->
 <!--<script src="anime.min.js" type="text/javascript" charset="utf-8"></script> -->
 <!--<script src="fireworks.js" type="text/javascript" charset="utf-8"></script> -->
-
 </html>
